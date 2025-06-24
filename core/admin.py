f44ef21ffd0cfad28f91f118a592056c89ff3c15
andrecris2003 <andreacristofaro003@gmail.com>
2025-06-24 14:32:19 +0200

    feat: Aggiungi serializer per Nationality e Player, aggiorna importazioni in admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import League, Team
+from .models import League, Team, Nationality, Player
 
 admin.site.register(League)
 admin.site.register(Team)
+admin.site.register(Nationality)
+admin.site.register(Player)
--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import League, Team
+from .models import League, Team, Nationality, Player
 
 
 class LeagueSerializer(serializers.ModelSerializer):
@@ -12,3 +12,26 @@ class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = '__all__'
+
+
+class NationalitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nationality
+        fields = '__all__'
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    main_nationality = NationalitySerializer()
+    other_nationalities = NationalitySerializer(many=True)
+    team = TeamSerializer()
+    old_team_name = TeamSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+    def create(self, validated_data):
+        main_nationality_data = validated_data.pop('main_nationality')
+        other_nationalities_data = validated_data.pop('other_nationalities', [])
+        team_data = validated_data.pop('team')
+        old_team_name_data = validated_data.pop('old_team_name', [])
