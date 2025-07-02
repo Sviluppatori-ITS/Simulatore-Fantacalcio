c65dd9f1330d91eaedbb42278ccf9cf353a047c0
andrecris2003 <andreacristofaro003@gmail.com>
2025-07-02 09:47:53 +0200

    refactor: Modifica il setUp del test per utilizzare setUpTestData e gestire le migrazioni

--- core/tests.py
+++ core/tests.py
@@ -1,12 +1,21 @@
 from django.test import TestCase
-from .models import Player, Person, Nationality
+from django.core.management import call_command
+from django.db import connection
+from .models import Nationality, Person, Player
 
 
 class PlayerModelTest(TestCase):
-    def setUp(self):
-        nationality = Nationality.objects.create(name="Italia", code="ITA")
-        person = Person.objects.create(name="Mario", surname="Rossi", birth_date="1995-05-01", main_nationality=nationality)
-        self.player = Player.objects.create(person=person, main_nationality=nationality)
+    @classmethod
+    def setUpTestData(cls):
+        # Crea migrazioni se necessario
+        if not connection.introspection.table_names():
+            call_command('makemigrations', verbosity=0)
+        # Esegui migrazioni (solo necessario se usi workaround specifici)
+        call_command('migrate', verbosity=0)
+
+        cls.nationality = Nationality.objects.create(name="Italia", code="ITA")
+        cls.person = Person.objects.create(name="Mario", surname="Rossi", birth_date="1995-05-01", main_nationality=cls.nationality)
+        cls.player = Player.objects.create(person=cls.person, main_nationality=cls.nationality)
 
     def test_default_overall(self):
         self.assertEqual(self.player.overall, 50)
