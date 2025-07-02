3fb95574324cdc99e2331f8b2ae8e12bf70d2225
andrecris2003 <andreacristofaro003@gmail.com>
2025-07-02 09:32:12 +0200

    feat: Aggiungi test per il modello Player e configura il setUp

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Player, Person, Nationality
 
-# Create your tests here.
+
+class PlayerModelTest(TestCase):
+    def setUp(self):
+        nationality = Nationality.objects.create(name="Italia", code="ITA")
+        person = Person.objects.create(name="Mario", surname="Rossi", birth_date="1995-05-01", main_nationality=nationality)
+        self.player = Player.objects.create(person=person, main_nationality=nationality)
+
+    def test_default_overall(self):
+        self.assertEqual(self.player.overall, 50)
