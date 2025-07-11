0a33cb1a7473ecaaf7e02f29da119cfd210dd205
andrecris2003 <andreacristofaro003@gmail.com>
2025-07-11 08:13:47 +0000

    feat: aggiunto router per nationality

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from .views import RegisterView
 from rest_framework.routers import DefaultRouter
-from .views import LeagueViewSet, TeamViewSet
+from .views import LeagueViewSet, TeamViewSet, NationalityViewSet
 
 router = DefaultRouter()
 router.register(r'leagues', LeagueViewSet)
 router.register(r'teams', TeamViewSet)
+router.register(r'nationalities', NationalityViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
