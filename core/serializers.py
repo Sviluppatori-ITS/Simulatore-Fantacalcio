bcd7a7778ff529e9e798f5bab89afbb7a264fb0e
andrecris2003 <andreacristofaro003@gmail.com>
2025-07-14 09:28:16 +0200

    feat: registrati i viewset nel ruter del core

--- core/serializers.py
+++ core/serializers.py
@@ -15,10 +15,39 @@ class TeamSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class ContinentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Continent
+        fields = '__all__'
+
+
+class ContinentNestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Continent
+        fields = ['id', 'name', 'code']  # o altri campi utili
+
+
 class NationalitySerializer(serializers.ModelSerializer):
+    # Se passi un id, userà quello. Se passi un oggetto, lo crea se non esistente.
+    continent = serializers.PrimaryKeyRelatedField(
+        queryset=Continent.objects.all(),
+        required=False,
+        allow_null=True
+    )
+    # In lettura, mostriamo i dettagli
+    continent_info = ContinentNestedSerializer(source='continent', read_only=True)
+
     class Meta:
         model = Nationality
-        fields = '__all__'
+        fields = ['id', 'name', 'code', 'continent', 'continent_info']
+
+    def create(self, validated_data):
+        continent = validated_data.pop('continent', None)
+        # Se continent è un dict, allora è stato passato un oggetto
+        if isinstance(continent, dict):
+            continent = Continent.objects.create(**continent)
+        nationality = Nationality.objects.create(continent=continent, **validated_data)
+        return nationality
 
 
 class PlayerSerializer(serializers.ModelSerializer):
@@ -36,12 +65,6 @@ class PlayerSerializer(serializers.ModelSerializer):
         other_nationalities_data = validated_data.pop('other_nationalities', [])
         team_data = validated_data.pop('team')
         old_team_name_data = validated_data.pop('old_team_name', [])
-
-
-class ContinentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Continent
-        fields = '__all__'
 
 
 class PlayerStatisticsSerializer(serializers.ModelSerializer):
--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from .views import RegisterView
 from rest_framework.routers import DefaultRouter
-from .views import LeagueViewSet, TeamViewSet, NationalityViewSet
+from .views import LeagueViewSet, TeamViewSet, NationalityViewSet, ContinentViewSet, TrophyViewSet, TournamentStructureViewSet, SeasonViewSet
 
 router = DefaultRouter()
 router.register(r'leagues', LeagueViewSet)
 router.register(r'teams', TeamViewSet)
 router.register(r'nationalities', NationalityViewSet)
+router.register(r'continent', ContinentViewSet)
+router.register(r'season', SeasonViewSet)
+router.register(r'tounament_structure', TournamentStructureViewSet)
+router.register(r'trophy', TrophyViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
