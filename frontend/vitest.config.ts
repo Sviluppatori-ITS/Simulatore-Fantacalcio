3949d13de45ef0a95a362ec06fd351f168914313
andrecris2003 <andreacristofaro003@gmail.com>
2025-07-16 16:23:28 +0200

    fix: cambiati alcuni config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: './src/setupTests.ts',
+        reporters: ['default', ['junit', { outputFile: 'frontend/test-results/results.xml' }]],
     },
 });
