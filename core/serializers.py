d3b70ce3dede32b48ff7facc1234fd950abfde10
andrecris2003 <andreacristofaro003@gmail.com>
2025-07-16 11:23:01 +0200

    feat: creato il MatchSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,35 +1,42 @@
 from rest_framework import serializers
-from .models import League, Team, Nationality, Player, Continent, PlayerStatistics, Season, Trophy, TournamentStructure, Person
+# from .models import League, Team, Nationality, Player, Continent, PlayerStatistics, Season, Trophy, TournamentStructure, Person
+from . import models
 from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
 
 
 class LeagueSerializer(serializers.ModelSerializer):
     class Meta:
-        model = League
+        model = models.League
         fields = '__all__'
 
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Team
+        model = models.Team
         fields = '__all__'
 
 
 class TeamNestedSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Team
+        model = models.Team
         fields = ['id', 'name', 'code']
 
 
 class ContinentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Continent
+        model = models.Continent
         fields = '__all__'
 
 
 class ContinentNestedSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Continent
+        model = models.Continent
         fields = ['id', 'name', 'code']  # o altri campi utili
 
 
@@ -38,7 +45,7 @@ class NationalitySerializer(serializers.ModelSerializer):
     continent = serializers.SerializerMethodField()
 
     class Meta:
-        model = Nationality
+        model = models.Nationality
         fields = ['id', 'name', 'code', 'continent', 'continent_info']
 
     def get_continent(self, obj):
@@ -48,7 +55,7 @@ class NationalitySerializer(serializers.ModelSerializer):
         if isinstance(data, dict):
             continent_data = data.pop('continent', None)
             if continent_data and isinstance(continent_data, dict):
-                continent, _ = Continent.objects.get_or_create(**continent_data)
+                continent, _ = models.Continent.objects.get_or_create(**continent_data)
                 data['continent'] = continent.id
             else:
                 data['continent'] = continent_data
@@ -57,12 +64,12 @@ class NationalitySerializer(serializers.ModelSerializer):
 
 class PersonSerializer(serializers.ModelSerializer):
     main_nationality = serializers.PrimaryKeyRelatedField(
-        queryset=Nationality.objects.all(),
+        queryset=models.Nationality.objects.all(),
         required=False,
         allow_null=True
     )
     other_nationalities = serializers.PrimaryKeyRelatedField(
-        queryset=Nationality.objects.all(),
+        queryset=models.Nationality.objects.all(),
         many=True,
         required=False
     )
@@ -72,7 +79,7 @@ class PersonSerializer(serializers.ModelSerializer):
     other_nationalities_info = NationalitySerializer(source='other_nationalities', many=True, read_only=True)
 
     class Meta:
-        model = Person
+        model = models.Person
         fields = [
             'id', 'name', 'birth_date',
             'main_nationality', 'other_nationalities',
@@ -81,7 +88,7 @@ class PersonSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         other_nationalities_data = validated_data.pop('other_nationalities', [])
-        person = Person.objects.create(**validated_data)
+        person = models.Person.objects.create(**validated_data)
         person.other_nationalities.set(other_nationalities_data)
         return person
 
@@ -101,7 +108,7 @@ class PlayerSerializer(serializers.ModelSerializer):
     old_team_name = TeamNestedSerializer(many=True, read_only=True)
 
     class Meta:
-        model = Player
+        model = models.Player
         fields = '__all__'
 
     def create(self, validated_data):
@@ -115,12 +122,12 @@ class PlayerSerializer(serializers.ModelSerializer):
 
         # Gestione TEAM
         if isinstance(team_data, dict):
-            team, _ = Team.objects.get_or_create(**team_data)
+            team, _ = models.Team.objects.get_or_create(**team_data)
         else:
-            team = Team.objects.get(id=team_data)
+            team = models.Team.objects.get(id=team_data)
 
         # Crea PLAYER
-        player = Player.objects.create(
+        player = models.Player.objects.create(
             person=person,
             team=team,
             **validated_data
@@ -131,29 +138,63 @@ class PlayerSerializer(serializers.ModelSerializer):
 
 class PlayerStatisticsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PlayerStatistics
+        model = models.PlayerStatistics
         fields = '__all__'
 
 
 class SeasonSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Season
+        model = models.Season
         fields = '__all__'
 
 
 class TrophySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Trophy
+        model = models.Trophy
         fields = '__all__'
 
 
 class TournamentStructureSerializer(serializers.ModelSerializer):
     class Meta:
-        model = TournamentStructure
+        model = models.TournamentStructure
         fields = '__all__'
 
 
-class UserSerializer(serializers.ModelSerializer):
+class FormationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
+        model = models.Formations
+        fields = '__all__'
+
+
+class DefaultFormationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.DefaultFormation
+        fields = '__all__'
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    home_team = TeamSerializer()
+    away_team = TeamSerializer()
+
+    class Meta:
+        model = models.Match
+        fields = '__all__'
+
+    def create(self, validated_data):
+        home_team_data = validated_data.pop('home_team')
+        away_team_data = validated_data.pop('away_team')
+
+        try:
+            home_team = models.Team.objects.get(**home_team_data)
+        except models.Team.DoesNotExist:
+            raise serializers.ValidationError({'home_team': 'Team non trovato'})
+
+        try:
+            away_team = models.Team.objects.get(**away_team_data)
+        except models.Team.DoesNotExist:
+            raise serializers.ValidationError({'away_team': 'Team non trovato'})
+
+        validated_data['home_team'] = home_team
+        validated_data['away_team'] = away_team
+
+        return models.Match.objects.create(**validated_data)
